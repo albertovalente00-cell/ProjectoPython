feea70ccbe4d8f01b99e404a88837aba213e4c38
albertokitala <252365009+albertovalente00-cell@users.noreply.github.com>
2026-06-05 17:19:23 +0100

    primeira versão do programa

---
+++ calculate.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+def calculate(lista):
+    if len(lista) != 9:
+        raise ValueError("A lista deve conter nove números")
+
+    matriz = np.array(lista).reshape(3, 3)
+
+    calculations = {
+        'mean': [
+            matriz.mean(axis=0).tolist(),
+            matriz.mean(axis=1).tolist(),
+            matriz.mean().tolist()
+        ],
+        'variance': [
+            matriz.var(axis=0).tolist(),
+            matriz.var(axis=1).tolist(),
+            matriz.var().tolist()
+        ],
+        'standard deviation': [
+            matriz.std(axis=0).tolist(),
+            matriz.std(axis=1).tolist(),
+            matriz.std().tolist()
+        ],
+        'max': [
+            matriz.max(axis=0).tolist(),
+            matriz.max(axis=1).tolist(),
+            matriz.max().tolist()
+        ],
+        'min': [
+            matriz.min(axis=0).tolist(),
+            matriz.min(axis=1).tolist(),
+            matriz.min().tolist()
+        ],
+        'sum': [
+            matriz.sum(axis=0).tolist(),
+            matriz.sum(axis=1).tolist(),
+            matriz.sum().tolist()
+        ]
+    }
+
+    return calculations
---
+++ main.py
@@ -0,0 +1,3 @@
+from calculate import calculate
+
+print(calculate([0,1,2,3,4,5,6,7,8]))
